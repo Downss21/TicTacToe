9e5c6c16e069325390c33d683148eab28b94fde1
Sam Downs <downss21@stxtigers.com>
2021-03-24 14:05:49 -0400

    Method Madness Start
    wrote a demo program to get a feel for what end product should look like

---
+++ TicTacToe/src/DemoMethodTTT.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class DemoMethodTTT {
+	
+	public static Scanner reader = new Scanner(System.in);
+	public static int input;
+
+	public static void main(String[] args) {
+		input = reader.nextInt();
+		if (input == 0)
+		{
+			method00();
+		}
+		if (input == 1)
+		{
+			method01();
+		}
+		if (input == 2)
+		{
+			method02();
+		}
+		if (input == 3)
+		{
+			method03();
+		}
+		if (input == 4)
+		{
+			method04();
+		}
+		if (input == 5)
+		{
+			method05();
+		}
+		if (input == 6)
+		{
+			method06();
+		}
+		if (input == 7)
+		{
+			method07();
+		}
+		if (input == 8)
+		{
+			method08();
+		}
+	}
+	
+	private static void method00()
+	{
+		input = reader.nextInt();
+		if (input == 0)
+		{
+			method00();
+		}
+		if (input == 1)
+		{
+			method01();
+		}
+		if (input == 2)
+		{
+			method02();
+		}
+		if (input == 3)
+		{
+			method03();
+		}
+		if (input == 4)
+		{
+			method04();
+		}
+		if (input == 5)
+		{
+			method05();
+		}
+		if (input == 6)
+		{
+			method06();
+		}
+		if (input == 7)
+		{
+			method07();
+		}
+		if (input == 8)
+		{
+			method08();
+		}
+	}
+	private static void method01()
+	{
+		
+	}
+	private static void method02()
+	{
+		
+	}
+	private static void method03()
+	{
+		
+	}
+	private static void method04()
+	{
+		
+	}
+	private static void method05()
+	{
+		
+	}
+	private static void method06()
+	{
+		
+	}
+	private static void method07()
+	{
+		
+	}
+	private static void method08()
+	{
+		
+	}
+}
--- TicTacToe/src/TTTGen.java
+++ TicTacToe/src/TTTGen.java
@@ -13,154 +13,16 @@ public class TTTGen {
 		writer.println("import java.util.Scanner;");
 		
 		writer.println("public class TicTacToe {");
-		writer.println("public static void main(String[] args) {");
-		writer.println("Scanner reader = new Scanner(System.in);");
+		writer.println("static Scanner reader = new Scanner(System.in);");
+		writer.println("static int input;");
 		
 		//main game nest
-		for (int i1 = 0; i1 < 9; i1++)
+		for (int i = 0; i < 9; i++)
 		{
-			char[] board1 = Arrays.copyOf(board, 9);
-			printBoard(writer, board1);
-			writer.println("System.out.print(\"Enter the space you want to play on X: \");");
-			printSelection(writer);
-			writer.println("int input = reader.nextInt();");
-			board1[i1] = 'X';
-			if(isWon(i1, board1))
+			for (int j = 0; j < 9; j++)
 			{
-				writer.println("System.out.print(\"X Won!\");");
-				continue;
+				
 			}
-			writer.println("if(input == "+i1+"); \n {");
-			for (int i2 = 1; i2 < 9; i2++)
-			{
-				char[] board2 = Arrays.copyOf(board1, 9);
-				printBoard(writer, board2);
-				writer.println("System.out.print(\"Enter the space you want to play on O: \");");
-				printSelection(writer);
-				writer.println("input = reader.nextInt();");
-				board2[i2] = 'O';
-				if(isWon(i2, board2))
-				{
-					writer.println("System.out.print(\"O Won!\");");
-					continue;
-				}
-				writer.println("if(input == "+i2+"); \n {");
-				for (int i3 = 2; i3 < 9; i3++)
-				{
-					char[] board3 = Arrays.copyOf(board2, 9);
-					printBoard(writer, board3);
-					writer.println("System.out.print(\"Enter the space you want to play on X: \");");
-					printSelection(writer);
-					writer.println("input = reader.nextInt();");
-					board3[i3] = 'X';
-					if(isWon(i3, board3))
-					{
-						writer.println("System.out.print(\"X Won!\");");
-						continue;
-					}
-					writer.println("if(input == "+i3+"); \n {");
-					for (int i4 = 3; i4 < 9; i4++)
-					{
-						char[] board4 = Arrays.copyOf(board3, 9);
-						printBoard(writer, board4);
-						writer.println("System.out.print(\"Enter the space you want to play on O: \");");
-						printSelection(writer);
-						writer.println("input = reader.nextInt();");
-						board4[i4] = 'O';
-						if(isWon(i4, board4))
-						{
-							writer.println("System.out.print(\"O Won!\");");
-							continue;
-						}
-						writer.println("if(input == "+i4+"); \n {");
-						for (int i5 = 4; i5 < 9; i5++)
-						{
-							char[] board5 = Arrays.copyOf(board4, 9);
-							printBoard(writer, board5);
-							writer.println("System.out.print(\"Enter the space you want to play on X: \");");
-							printSelection(writer);
-							writer.println("input = reader.nextInt();");
-							board5[i5] = 'X';
-							if(isWon(i5, board5))
-							{
-								writer.println("System.out.print(\"X Won!\");");
-								continue;
-							}
-							writer.println("if(input == "+i5+"); \n {");
-							for (int i6 = 5; i6 < 9; i6++)
-							{
-								char[] board6 = Arrays.copyOf(board5, 9);
-								printBoard(writer, board6);
-								writer.println("System.out.print(\"Enter the space you want to play on O: \");");
-								printSelection(writer);
-								writer.println("input = reader.nextInt();");
-								board6[i6] = 'O';
-								if(isWon(i6, board6))
-								{
-									writer.println("System.out.print(\"O Won!\");");
-									continue;
-								}
-								writer.println("if(input == "+i6+"); \n {");
-								for (int i7 = 6; i7 < 9; i7++)
-								{
-									char[] board7 = Arrays.copyOf(board6, 9);
-									printBoard(writer, board7);
-									writer.println("System.out.print(\"Enter the space you want to play on X: \");");
-									printSelection(writer);
-									writer.println("input = reader.nextInt();");
-									board7[i7] = 'X';
-									if(isWon(i7, board7))
-									{
-										writer.println("System.out.print(\"X Won!\");");
-										continue;
-									}
-									writer.println("if(input == "+i7+"); \n {");
-									for (int i8 = 7; i8 < 9; i8++)
-									{
-										char[] board8 = Arrays.copyOf(board7, 9);
-										printBoard(writer, board8);
-										writer.println("System.out.print(\"Enter the space you want to play on O: \");");
-										printSelection(writer);
-										writer.println("input = reader.nextInt();");
-										board8[i8] = 'O';
-										if(isWon(i8, board8))
-										{
-											writer.println("System.out.print(\"O Won!\");");
-											continue;
-										}
-										writer.println("if(input == "+i8+"); \n {");
-										for (int i9 = 8; i9 < 9; i9++)
-										{
-											char[] board9 = Arrays.copyOf(board8, 9);
-											printBoard(writer, board9);
-											writer.println("System.out.print(\"Enter the space you want to play on X: \");");
-											printSelection(writer);
-											writer.println("input = reader.nextInt();");
-											board9[i9] = 'X';
-											if(isWon(i9, board9))
-											{
-												writer.println("System.out.print(\"X Won!\");");
-												continue;
-											}
-											writer.println("if(input == "+i9+"); \n {");
-											//writer.println("System.out.println("+i1+" "+i2+" "+i3+" "+i4+" "+i5+" "+i6+" "+i7+" "+i8+" "+i9+");");
-											writer.println("}");
-										}
-										writer.println("}");
-									}
-									writer.println("}");
-								}
-								writer.println("}");
-							}
-							writer.println("}");
-						}
-						writer.println("}");
-					}
-					writer.println("}");
-				}
-				writer.println("}");
-			}
-			writer.println("}");
 		}
 		writer.print("}\n}");
 		writer.close();
