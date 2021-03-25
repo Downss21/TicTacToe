efa8736b22339d896cf7f79d1464f1b43190c1f0
downss21 <Downss21@stxtigers.com>
2021-03-25 12:11:55 -0400

    # WARNING: head commit changed in the meantime
    aaaaaaaaa

--- TicTacToe/src/PlayTTT.java
+++ TicTacToe/src/PlayTTT.java
@@ -1,45 +1,73 @@
-
-// Example 12.3
 import java.util.Scanner;
 import java.util.Random;
 
-public class PlayTTT {
-	public static void main(String[]args){ // Instantiate a keyboard scanner and a board
-		Scanner reader = new Scanner(System.in); TTTBoard board = new TTTBoard();
-		// Display the empty board
-		System.out.println(board);
-		// Randomly decide who goes first
-		Random gen = new Random(); char player; if (gen.nextInt(2) == 1)
-			player = 'O';
-		else
-			player = 'X';
-		// Loop while there is no winner and the board is not full
-		while (board.getWinner() == '-' && !board.full()){
+public class PlayTTT
+{
+  public static void main(String[] args)
+  {
+    Scanner reader = new Scanner(System.in);
+    TTTBoard board = new TTTBoard();
 
-			// Prompt the user for a move
-			System.out.println(player + "'s turn"); System.out.print("Enter the row and column[1-3, space, 1-3]: ");
-					// Read the move
-					int row = reader.nextInt(); int column = reader.nextInt();
-					boolean success = board.placeXorO(player, row, column); if (! success)
-					System.out.println("Error: cell already occupied!"); else{
-					/*
-					 * Attempt the move 
-					 * If the move is illegal
-					 * display an error message 
-					 * Else
-					 * display the board and switch players
-					 */
-					System.out.println(board); if (player == 'X')
-						player = 'O';
-					else
-						player = 'X';
-					// Display results
-					char winner = board.getWinner(); if (winner != '-')
-						System.out.println(winner + "s win!");
-					else
-						System.out.println("It's a draw!");
+    System.out.println(board);
 
-		}
-		}
-	}
+    Random gen = new Random();
+    char player;
+    if(gen.nextInt(2) == 1)
+    {
+      player = 'O';
+    }
+    else
+    {
+      player = 'X';
+    }
+
+    while(board.getWinner() == '-' && !board.full())
+    {
+      System.out.println(player + "'s turn");
+      System.out.println("Enter the row and column[1-3, space, 1-3]: ");
+
+      int row = reader.nextInt();
+      int col = reader.nextInt();
+
+      //Check input
+      if(row < 1 || row > 3 || col < 1 || col > 3)
+      {
+        System.out.println("Enter numbers between 1 and 3 separated by a space!");
+        continue;  //restart loop
+      }
+
+      boolean success = board.placeXorO(player,row,col);
+      if(!success)
+      {
+        System.out.println("Error: cell already occupied!");
+      }
+      else
+      {
+        System.out.println(board);
+        if(player == 'X')
+        {
+          player = 'O';
+        }
+        else
+        {
+          player = 'X';
+        }
+
+
+      }
+
+    }
+
+    char winner = board.getWinner();
+    if(winner != '-')
+    {
+      System.out.println(winner + "'s win!");
+    }
+    else
+    {
+      System.out.println("It's a draw!");
+    }
+
+
+  }
 }
