ad62f4a0e5f8ebdf3db1dfedb589117aac5e585f
Sam Downs <downss21@stxtigers.com>
2021-03-27 18:57:38 -0400

    bad git practice

--- TicTacToe/src/TTTBoard.java
+++ TicTacToe/src/TTTBoard.java
@@ -1,6 +1,5 @@
-
-// Example 12.3 (continued)
 public class TTTBoard {
+	// Holds values of each square, 3x3 array
 	private char[][] board;
 
 	public TTTBoard() {
@@ -10,40 +9,85 @@ public class TTTBoard {
 
 	public void reset() {
 		for (int row = 0; row < 3; row++)
-			for (int column = 0; column < 3; column++)
-				board[row][column] = '-';
+		{
+			for (int col = 0; col < 3; col++)
+			{
+				board[row][col] = '-';
+			}
+		}
 	}
 
 	public String toString() {
 		String result = "\n";
 		for (int row = 0; row < 3; row++)
 		{
-
-			for (int column = 0; column < 3; column++)
-				result += board[row][column] + " ";
-			result += "\n";
+			for (int col = 0; col < 3; col++)
+			{
+				result = result + board[row][col];
+			}
+			result = result + "\n";
 		}
 		return result;
 	}
 
-	public boolean placeXorO(char player, int row, int column) {
-		if (board[row - 1][column - 1] == '-')
+	public boolean placeXorO(char player, int row, int col) {
+		if (board[row - 1][col - 1] == '-')
 		{
-			board[row - 1][column - 1] = player;
+			board[row - 1][col - 1] = player;
 			return true;
 		}
-		else return false;
+		else
+		{
+			return false;
+		}
 	}
-	// Methods to test for fullness and to check for a winner // go here
 
 	public char getWinner() {
-		// TODO Auto-generated method stub
-		return 0
+		String lines[] = new String[8];
+		for (int i = 0; i < board.length; i++)
+		{
+			String lineH = "";
+			String lineV = "";
+			for (int j = 0; j < board[i].length; j++)
+			{
+				lineH += board[i][j];
+				lineV += board[j][i];
+			}
+			lines[2 * i] = lineH;
+			lines[(2 * i + 1)] = lineV;
+		}
+		lines[6] = Character.toString(board[0][0]) + board[1][1] + board[2][2];
+		lines[7] = Character.toString(board[0][2]) + board[1][1] + board[3][0];
+
+		for (int i = 0; i < lines.length; i++)
+		{
+			if (lines[i].equals("XXX"))
+			{
+				return 'X';
+			}
+			else
+			{
+				if (lines[i].equals("OOO"))
+				{
+					return 'O';
+				}
+			}
+		}
+		return '-';
 	}
 
 	public boolean full() {
-		// TODO Auto-generated method stub
-		return false;
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[i].length; j++)
+			{
+				if (board[i][j] == '-')
+				{
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }
